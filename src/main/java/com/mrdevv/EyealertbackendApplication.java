10d0033b56a58228987bf98dfa50ad9159b8e4d0
Miguel Vega Perez <aries_242015@hotmail.com>
2025-05-15 13:04:47 -0500

    fix timezone

--- src/main/java/com/mrdevv/EyealertbackendApplication.java
+++ src/main/java/com/mrdevv/EyealertbackendApplication.java
@@ -1,7 +1,11 @@
 package com.mrdevv;
 
+import jakarta.annotation.PostConstruct;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.time.LocalDateTime;
+import java.time.ZoneId;
 
 @SpringBootApplication
 public class EyealertbackendApplication {
@@ -10,4 +14,11 @@ public class EyealertbackendApplication {
 		SpringApplication.run(EyealertbackendApplication.class, args);
 	}
 
+	@PostConstruct
+	public void verificarZonaHoraria() {
+		System.out.println("Hora local en el backend: " +
+				LocalDateTime.now(ZoneId.systemDefault()));
+		System.out.println("Zona horaria de JVM: " +
+				ZoneId.systemDefault());
+	}
 }
